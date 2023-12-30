38e2165c92b0ec0e80ffd4c61a2d3233f5f02e0a
YouTee <80574565+YouTeeYouTee@users.noreply.github.com>
2023-12-31 00:02:57 +0100

    Update Plugin 1.0

---
+++ src/main/java/fr/youtee/waitplugin/Commands/WaitCommand.java
@@ -0,0 +1,46 @@
+package fr.youtee.waitplugin.Commands;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.List;
+
+import static fr.youtee.waitplugin.WaitPlugin.plugin;
+
+public class WaitCommand implements TabExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Vérifiez si la commande a été correctement utilisée
+        if (args.length != 2) {
+            sender.sendMessage("Utilisation : /wait <secondes> <commande>");
+            return true;
+        }
+
+        try {
+            int seconds = Integer.parseInt(args[0]);
+
+            // Créez une tâche BukkitRunnable pour retarder l'exécution de la commande
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    // Exécutez la commande spécifiée après le délai
+                    sender.getServer().dispatchCommand(sender, args[1]);
+                }
+            }.runTaskLater(plugin, seconds * 20L); // 20 ticks par seconde
+
+            sender.sendMessage("La commande sera exécutée après " + seconds + " secondes.");
+        } catch (NumberFormatException e) {
+            sender.sendMessage("Veuillez spécifier un nombre valide de secondes.");
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        // Pour l'instant tu peux juste mettre null et tu pourras le compléter après vu que c'est optionnel
+        return null;
+    }
+}
---
+++ src/main/java/fr/youtee/waitplugin/WaitPlugin.java
@@ -0,0 +1,18 @@
+package fr.youtee.waitplugin;
+
+import fr.youtee.waitplugin.Commands.WaitCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WaitPlugin extends JavaPlugin {
+
+    public static WaitPlugin plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        this.getLogger().info("WaitPlugin 1.1 - Enabled!");
+        getCommand("wait").setExecutor(new WaitCommand());
+        // Plugin startup logic
+    }
+
+}
